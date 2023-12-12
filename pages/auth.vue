4f5c9acf2c9da7053a44a72fa88b2d7312084a70
MrDaseMan <48094227+MrDaseMan@users.noreply.github.com>
2023-12-12 15:48:21 +0400

    show error

--- pages/auth.vue
+++ pages/auth.vue
@@ -127,7 +127,7 @@ const auth = () => {
 
                         isError.value = true;
                         isFetching.value = false;
-                        error_text.value = "Произошла ошибка при отправке данных на сервер!";
+                        error_text.value = "Произошла ошибка при отправке данных на сервер: " + JSON.stringify(result.error);
 
                         setTimeout(() => {
                             isError.value = false;
