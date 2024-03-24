1f7b53b5dbb63df5bb65d4e253e8f14db8fe5446
MrDaseMan <mrdaseman@outlook.com>
2024-03-24 17:31:15 +0400

    removed custom imports

--- composables/index.ts
+++ composables/index.ts
@@ -1,18 +1,18 @@
-export { usePostGroups } from "./requests/POST/usePostGroups";
-export { usePostProgram } from "./requests/POST/usePostProgram";
-export { usePostProgramByName } from "./requests/POST/usePostProgramByName";
-export { usePostAccessToken } from "./requests/POST/usePostAccessToken";
+// export { usePostGroups } from "./requests/POST/usePostGroups";
+// export { usePostProgram } from "./requests/POST/usePostProgram";
+// export { usePostProgramByName } from "./requests/POST/usePostProgramByName";
+// export { usePostAccessToken } from "./requests/POST/usePostAccessToken";
 
-// VK
-export { useVkGetGroups } from "./requests/vk/GET/useVkGetGroups";
-export { useVkGetSubscriptions } from "./requests/vk/GET/useVkGetSubscriptions";
-export { useVkGetUser } from "./requests/vk/GET/useVkGetUser";
-export { useVkGetStudying } from "./requests/vk/GET/useVkGetStudying";
+// // VK
+// export { useVkGetGroups } from "./requests/vk/GET/useVkGetGroups";
+// export { useVkGetSubscriptions } from "./requests/vk/GET/useVkGetSubscriptions";
+// export { useVkGetUser } from "./requests/vk/GET/useVkGetUser";
+// export { useVkGetStudying } from "./requests/vk/GET/useVkGetStudying";
 
-export { useResults } from "./store/useResults";
-export { useUser } from "./store/useUser";
-export { useGroup } from "./store/useGroup";
+// export { useResults } from "./store/useResults";
+// export { useUser } from "./store/useUser";
+// export { useGroup } from "./store/useGroup";
 
-export { useCustomError } from "./utils/useCustomError";
-export { useToken } from "./utils/useToken"
-export { useUserID } from "./utils/useUserID"
+// export { useCustomError } from "./utils/useCustomError";
+// export { useToken } from "./utils/useToken"
+// export { useUserID } from "./utils/useUserID"
